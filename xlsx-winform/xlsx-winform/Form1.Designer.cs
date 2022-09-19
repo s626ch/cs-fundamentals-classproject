2ab76d57b527e10cd0be76ec5963047cc8a019f4
smolyoshino <coolguygameplayer52@gmail.com>
2022-09-19 09:18:50 -0400

    xlsx-winform commit
    winforms application with a 4x4 view to read and write to an xlsx file

---
+++ xlsx-winform/xlsx-winform/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace xlsx_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SprA1 = new System.Windows.Forms.TextBox();
+            this.SprB1 = new System.Windows.Forms.TextBox();
+            this.SprC1 = new System.Windows.Forms.TextBox();
+            this.SprD1 = new System.Windows.Forms.TextBox();
+            this.SprA2 = new System.Windows.Forms.TextBox();
+            this.SprB2 = new System.Windows.Forms.TextBox();
+            this.SprC2 = new System.Windows.Forms.TextBox();
+            this.SprD2 = new System.Windows.Forms.TextBox();
+            this.SprA3 = new System.Windows.Forms.TextBox();
+            this.SprB3 = new System.Windows.Forms.TextBox();
+            this.SprC3 = new System.Windows.Forms.TextBox();
+            this.SprD3 = new System.Windows.Forms.TextBox();
+            this.SprA4 = new System.Windows.Forms.TextBox();
+            this.SprB4 = new System.Windows.Forms.TextBox();
+            this.SprC4 = new System.Windows.Forms.TextBox();
+            this.SprD4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SprA1
+            // 
+            this.SprA1.Location = new System.Drawing.Point(31, 23);
+            this.SprA1.Name = "SprA1";
+            this.SprA1.Size = new System.Drawing.Size(100, 20);
+            this.SprA1.TabIndex = 0;
+            // 
+            // SprB1
+            // 
+            this.SprB1.Location = new System.Drawing.Point(137, 23);
+            this.SprB1.Name = "SprB1";
+            this.SprB1.Size = new System.Drawing.Size(100, 20);
+            this.SprB1.TabIndex = 1;
+            // 
+            // SprC1
+            // 
+            this.SprC1.Location = new System.Drawing.Point(243, 23);
+            this.SprC1.Name = "SprC1";
+            this.SprC1.Size = new System.Drawing.Size(100, 20);
+            this.SprC1.TabIndex = 2;
+            // 
+            // SprD1
+            // 
+            this.SprD1.Location = new System.Drawing.Point(349, 23);
+            this.SprD1.Name = "SprD1";
+            this.SprD1.Size = new System.Drawing.Size(100, 20);
+            this.SprD1.TabIndex = 3;
+            // 
+            // SprA2
+            // 
+            this.SprA2.Location = new System.Drawing.Point(31, 50);
+            this.SprA2.Name = "SprA2";
+            this.SprA2.Size = new System.Drawing.Size(100, 20);
+            this.SprA2.TabIndex = 4;
+            // 
+            // SprB2
+            // 
+            this.SprB2.Location = new System.Drawing.Point(137, 50);
+            this.SprB2.Name = "SprB2";
+            this.SprB2.Size = new System.Drawing.Size(100, 20);
+            this.SprB2.TabIndex = 5;
+            // 
+            // SprC2
+            // 
+            this.SprC2.Location = new System.Drawing.Point(243, 50);
+            this.SprC2.Name = "SprC2";
+            this.SprC2.Size = new System.Drawing.Size(100, 20);
+            this.SprC2.TabIndex = 6;
+            // 
+            // SprD2
+            // 
+            this.SprD2.Location = new System.Drawing.Point(349, 50);
+            this.SprD2.Name = "SprD2";
+            this.SprD2.Size = new System.Drawing.Size(100, 20);
+            this.SprD2.TabIndex = 7;
+            // 
+            // SprA3
+            // 
+            this.SprA3.Location = new System.Drawing.Point(31, 77);
+            this.SprA3.Name = "SprA3";
+            this.SprA3.Size = new System.Drawing.Size(100, 20);
+            this.SprA3.TabIndex = 8;
+            // 
+            // SprB3
+            // 
+            this.SprB3.Location = new System.Drawing.Point(137, 77);
+            this.SprB3.Name = "SprB3";
+            this.SprB3.Size = new System.Drawing.Size(100, 20);
+            this.SprB3.TabIndex = 9;
+            // 
+            // SprC3
+            // 
+            this.SprC3.Location = new System.Drawing.Point(243, 77);
+            this.SprC3.Name = "SprC3";
+            this.SprC3.Size = new System.Drawing.Size(100, 20);
+            this.SprC3.TabIndex = 10;
+            // 
+            // SprD3
+            // 
+            this.SprD3.Location = new System.Drawing.Point(349, 77);
+            this.SprD3.Name = "SprD3";
+            this.SprD3.Size = new System.Drawing.Size(100, 20);
+            this.SprD3.TabIndex = 11;
+            // 
+            // SprA4
+            // 
+            this.SprA4.Location = new System.Drawing.Point(31, 104);
+            this.SprA4.Name = "SprA4";
+            this.SprA4.Size = new System.Drawing.Size(100, 20);
+            this.SprA4.TabIndex = 12;
+            // 
+            // SprB4
+            // 
+            this.SprB4.Location = new System.Drawing.Point(137, 104);
+            this.SprB4.Name = "SprB4";
+            this.SprB4.Size = new System.Drawing.Size(100, 20);
+            this.SprB4.TabIndex = 13;
+            // 
+            // SprC4
+            // 
+            this.SprC4.Location = new System.Drawing.Point(243, 104);
+            this.SprC4.Name = "SprC4";
+            this.SprC4.Size = new System.Drawing.Size(100, 20);
+            this.SprC4.TabIndex = 14;
+            // 
+            // SprD4
+            // 
+            this.SprD4.Location = new System.Drawing.Point(349, 104);
+            this.SprD4.Name = "SprD4";
+            this.SprD4.Size = new System.Drawing.Size(100, 20);
+            this.SprD4.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Write to File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Read from File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(349, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "D";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 168);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SprD4);
+            this.Controls.Add(this.SprC4);
+            this.Controls.Add(this.SprB4);
+            this.Controls.Add(this.SprA4);
+            this.Controls.Add(this.SprD3);
+            this.Controls.Add(this.SprC3);
+            this.Controls.Add(this.SprB3);
+            this.Controls.Add(this.SprA3);
+            this.Controls.Add(this.SprD2);
+            this.Controls.Add(this.SprC2);
+            this.Controls.Add(this.SprB2);
+            this.Controls.Add(this.SprA2);
+            this.Controls.Add(this.SprD1);
+            this.Controls.Add(this.SprC1);
+            this.Controls.Add(this.SprB1);
+            this.Controls.Add(this.SprA1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "XLSX Example app";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SprA1;
+        private System.Windows.Forms.TextBox SprB1;
+        private System.Windows.Forms.TextBox SprC1;
+        private System.Windows.Forms.TextBox SprD1;
+        private System.Windows.Forms.TextBox SprA2;
+        private System.Windows.Forms.TextBox SprB2;
+        private System.Windows.Forms.TextBox SprC2;
+        private System.Windows.Forms.TextBox SprD2;
+        private System.Windows.Forms.TextBox SprA3;
+        private System.Windows.Forms.TextBox SprB3;
+        private System.Windows.Forms.TextBox SprC3;
+        private System.Windows.Forms.TextBox SprD3;
+        private System.Windows.Forms.TextBox SprA4;
+        private System.Windows.Forms.TextBox SprB4;
+        private System.Windows.Forms.TextBox SprC4;
+        private System.Windows.Forms.TextBox SprD4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ xlsx-winform/xlsx-winform/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Bibliography;
+using Excel = ClosedXML.Excel;
+
+namespace xlsx_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // read from file
+            string CurrentDir = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            CurrentDir += @"\sheet.xlsx";
+            var wb = new Excel.XLWorkbook(CurrentDir);
+            var ws = wb.Worksheets.Worksheet("Sheet1");
+            if (System.IO.File.Exists(CurrentDir)) {
+                TextBox[] FormA = new[] { SprA1, SprA2, SprA3, SprA4 };
+                TextBox[] FormB = new[] { SprB1, SprB2, SprB3, SprB4 };
+                TextBox[] FormC = new[] { SprC1, SprC2, SprC3, SprC4 };
+                TextBox[] FormD = new[] { SprD1, SprD2, SprD3, SprD4 };
+                Object[] SheetA = new[] { ws.Cell("A1").Value, ws.Cell("A2").Value, ws.Cell("A3").Value, ws.Cell("A4").Value };
+                Object[] SheetB = new[] { ws.Cell("B1").Value, ws.Cell("B2").Value, ws.Cell("B3").Value, ws.Cell("B4").Value };
+                Object[] SheetC = new[] { ws.Cell("C1").Value, ws.Cell("C2").Value, ws.Cell("C3").Value, ws.Cell("C4").Value };
+                Object[] SheetD = new[] { ws.Cell("D1").Value, ws.Cell("D2").Value, ws.Cell("D3").Value, ws.Cell("D4").Value };
+                var SpreadA = FormA.Zip(SheetA, (f,s) => new { FrmA = f, ShtA = s });
+                foreach (var fs in SpreadA)
+                {
+                    fs.FrmA.Text = Convert.ToString(fs.ShtA);
+                }
+                var SpreadB = FormB.Zip(SheetB, (f, s) => new { FrmB = f, ShtB = s });
+                foreach (var fs in SpreadB)
+                {
+                    fs.FrmB.Text = Convert.ToString(fs.ShtB);
+                }
+                var SpreadC = FormC.Zip(SheetC, (f, s) => new { FrmC = f, ShtC = s });
+                foreach (var fs in SpreadC)
+                {
+                    fs.FrmC.Text = Convert.ToString(fs.ShtC);
+                }
+                var SpreadD = FormD.Zip(SheetD, (f, s) => new { FrmD = f, ShtD = s });
+                foreach (var fs in SpreadD)
+                {
+                    fs.FrmD.Text = Convert.ToString(fs.ShtD);
+                }
+            } else
+            {
+                MessageBox.Show("File doesn't exist.","Error!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // write to file
+            string CurrentDir = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            CurrentDir += @"\sheet.xlsx";
+            var wb = new Excel.XLWorkbook(CurrentDir);
+            var ws = wb.Worksheets.Worksheet("Sheet1");
+            if (System.IO.File.Exists(CurrentDir))
+            {
+                TextBox[] FormA = new[] { SprA1, SprA2, SprA3, SprA4 };
+                TextBox[] FormB = new[] { SprB1, SprB2, SprB3, SprB4 };
+                TextBox[] FormC = new[] { SprC1, SprC2, SprC3, SprC4 };
+                TextBox[] FormD = new[] { SprD1, SprD2, SprD3, SprD4 };
+                String[] SheetA = new[] { "A1", "A2", "A3", "A4" };
+                String[] SheetB = new[] { "B1", "B2", "B3", "B4" };
+                String[] SheetC = new[] { "C1", "C2", "C3", "C4" };
+                String[] SheetD = new[] { "D1", "D2", "D3", "D4" };
+
+                // what the hell is this
+                for (int i = 0; i < FormA.Length; i++)
+                {
+                    ws.Cell(SheetA[i]).Value = FormA[i].Text;
+                }
+                for (int i = 0; i < FormB.Length; i++)
+                {
+                    ws.Cell(SheetB[i]).Value = FormB[i].Text;
+                }
+                for (int i = 0; i < FormC.Length; i++)
+                {
+                    ws.Cell(SheetC[i]).Value = FormC[i].Text;
+                }
+                for (int i = 0; i < FormD.Length; i++)
+                {
+                    ws.Cell(SheetD[i]).Value = FormD[i].Text;
+                }
+
+                wb.Save();
+            }
+            else
+            {
+                MessageBox.Show("File doesn't exist.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
