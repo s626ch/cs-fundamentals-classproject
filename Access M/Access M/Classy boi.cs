837a279c7b19829d6900c435d49991ee95155e0a
RadiantBlue223 <90406211+RadiantBlue223@users.noreply.github.com>
2022-09-22 08:45:04 -0400

    access modifiers

---
+++ Access M/Access M/Classy boi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_M
+{
+    internal class Classy_boi
+    { 
+        public static void Acmod()
+        {
+            Console.WriteLine("Congrats on the Modifier");
+            Console.ReadKey();
+        }
+    }
+}
